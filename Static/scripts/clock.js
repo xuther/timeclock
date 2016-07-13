0c4770074e78365a7d6db8ba0072778cb4719954
Joe Blodgett <t-joblod@microsoft.com>
2016-07-13 16:57:13 -0700

    Popup for clock-out

--- Static/scripts/clock.js
+++ Static/scripts/clock.js
@@ -12,5 +12,57 @@ function clockIn() {
     url: "/api/clockin",
     type: "POST",
     data: JSON.stringify(toSend)
+  });
+}
+
+console.log("test");
+$(document).ready(function() {
+  $("#clock-out").click(function() {
+    clockOut();
   })
+
+  $("#clock-in").click(function() {
+    clockIn();
+  })
+
+  $("#cancel-out").click(function() {
+    cancelOut();
+  })
+  $("#save-out").click(function() {
+    confirmOut();
+  })
+});
+
+function clockIn() {
+  confirmIn();
+}
+
+function clockOut() {
+  document.getElementById("out-popup").classList.toggle("fadeIn");
+  document.getElementById("out-popup").classList.toggle("fadeOut");
+
+  document.getElementById("overlay").classList.toggle("fadeIn");
+  document.getElementById("overlay").classList.toggle("fadeOut");
+}
+
+function cancelOut() {
+  document.getElementById("out-popup").classList.toggle("fadeOut");
+  document.getElementById("out-popup").classList.toggle("fadeIn");
+
+  document.getElementById("overlay").classList.toggle("fadeIn");
+  document.getElementById("overlay").classList.toggle("fadeOut");
+}
+
+function confirmOut() {
+  document.getElementById("out-popup").classList.toggle("fadeOut");
+  document.getElementById("out-popup").classList.toggle("fadeIn");
+
+  document.getElementById("overlay").classList.toggle("fadeIn");
+  document.getElementById("overlay").classList.toggle("fadeOut");
+
+  $("#current-status").html("out");
+}
+
+function confirmIn() {
+  $("#current-status").html("in");
 }
